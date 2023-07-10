1e4fbc1a0f62d8814f5cc7c1cc4f4a75ea60d062
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2023-07-10 20:08:32 +0200

    Created instance of WebScraper and executed methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { WebScraper } from './classes/WebScraper';
+
 (async () => {
-  console.log('Hello World!');
+  const webScraper = new WebScraper();
+
+  const movies = await webScraper.scrapeMoviesFromSelectedYear(new Date().getFullYear());
+  const deduplicatedMovies = webScraper.deduplicateMovies(movies);
+  const sortedMovies = webScraper.sortMoviesByRating(deduplicatedMovies, 'descending');
+  webScraper.saveMoviesToCsv(sortedMovies, 'movies.csv');
 })().catch(error => {
   console.error(`[ERROR] ${error.message || error}`);
   process.exit(1);
