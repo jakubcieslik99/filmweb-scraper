6a9875e38d6d14b3afb803ad46b1a2a71b769030
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2023-07-21 20:28:54 +0200

    Updated index.ts & dependencies

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ import { WebScraper } from './classes/WebScraper';
   const movies = await webScraper.scrapeMoviesFromSelectedYear(new Date().getFullYear());
   const deduplicatedMovies = webScraper.deduplicateMovies(movies);
   const sortedMovies = webScraper.sortMoviesByRating(deduplicatedMovies, 'descending');
-  webScraper.saveMoviesToCsv(sortedMovies, 'movies.csv');
+  await webScraper.saveMoviesToCsv(sortedMovies, 'movies.csv');
+
+  process.exit(0);
 })().catch(error => {
   console.error(`[ERROR] ${error.message || error}`);
   process.exit(1);
